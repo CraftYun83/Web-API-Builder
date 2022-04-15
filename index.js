8f7061c252fc18e44d649f26a9abe75f4a894c2d
FourMC <47018858+CraftYun83@users.noreply.github.com>
2022-04-15 17:28:25 +0800

    Update index.js
    Added support for server.js and MongoDB database validation.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jsontextele = document.getElementById('returnjson');
 const connectdbele = document.getElementById('connectdb');
 const clearbuttonele = document.getElementById('clearbutton');
 const exportbuttonele = document.getElementById('exportbutton');
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 var curelement = undefined;
 var patheventsubmit = undefined;
 var defaultpath = false
@@ -150,6 +151,16 @@ function jsontext() {
   }
 }
 
+function afterDBResponse(data) {
+  if (data) {
+    document.getElementById("structure-viewer").innerHTML += '<h1>Connected to: '+document.getElementById("cdbinput").value+'</h1>'
+    document.getElementById("top-bar").innerHTML = ""
+    connecteddb = true
+  } else {
+    alert("Connection to MongoDB server failed.")
+  }
+}
+
 function connectdb() {
   if (!connecteddb) {
     document.getElementById("top-bar").innerHTML = ""
@@ -159,9 +170,9 @@ function connectdb() {
       if (document.getElementById("cdbinput").value === "") {
         alert("You have to enter a valid URI before submitting!")
       } else {
-        document.getElementById("structure-viewer").innerHTML += '<h1>Connected to: '+document.getElementById("cdbinput").value+'</h1>'
-        document.getElementById("top-bar").innerHTML = ""
-        connecteddb = true
+        fetch('/valmongouri?uri='+Base64.encode(document.getElementById("cdbinput").value))
+          .then(response => response.json())
+          .then(data => afterDBResponse(data));
       }
     });
   } else {
